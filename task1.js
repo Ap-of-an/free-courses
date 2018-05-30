b50d720c8ab21a34f42e8e3fd699c33f0778ef6f
Ap-of-an <dmitry.virtyal@gmail.com>
2018-05-30 19:42:06 +0700

    Изменил решение первой задачи в связи с новым её пониманием.

--- task1.js
+++ task1.js
@@ -1,15 +1,11 @@
-process.argv.shift();
-process.argv.shift(); // удалить перед релизом
-
-var str = process.argv.join('').toLowerCase();
-if (str[str.length - 1] == '?') {
-  str = str.slice(0, -1);
+function isPalindrom( str ) {
+  str = str.split(' ').join('').toLowerCase();
+  if (str[str.length - 1] == '?') {
+    str = str.slice(0, -1);
+  }
+  for (let i = 0, j = str.length-1; i < j; i++, j--) {
+    if (str[i] != str[j]) { return "NO"; }
+  }
+  return "YES";
 }
-
-var flag = true;
-for (let i = 0, j = str.length - 1; i < j; i++, j--) {
-  if (str[i] != str[j]) { flag = false; break; }
-}
-
-if (flag) {  process.stdout.write("YES"); } 
-else { process.stdout.write("NO"); }
+process.stdout.write(isPalindrom(process.argv[2]));
