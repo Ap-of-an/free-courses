3c86cc10771104766cf29fa73966670984d79094
Ap-of-an <dmitry.virtyal@gmail.com>
2018-05-30 13:43:38 +0700

    Решение первой задачи

--- task1.js
+++ task1.js
@@ -1,3 +1,15 @@
-/* Task 1 */
+process.argv.shift();
+process.argv.shift(); // удалить перед релизом
 
-/* Put your code here */
+var str = process.argv.join('').toLowerCase();
+if (str[str.length - 1] == '?') {
+  str = str.slice(0, -1);
+}
+
+var flag = true;
+for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+  if (str[i] != str[j]) { flag = false; break; }
+}
+
+if (flag) {  process.stdout.write("YES"); } 
+else { process.stdout.write("NO"); }
