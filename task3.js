9bb7d8db2ff423b3bac5a359d1ca59acc28474f9
Ap-of-an <dmitry.virtyal@gmail.com>
2018-06-06 13:31:19 +0700

    Изменил алгоритм разделения строк на более короткий

--- task3.js
+++ task3.js
@@ -1,28 +1,7 @@
-function division(str) {
-  let gl = "aeiouаеёиоуыэюя",
-    cogl = "bcdfghjklmnpqrstvwxyzбвгджзйклмнпрстфхцчшщ",
-    cifr = "0123456789";
-  let a = [], b = [], c = [];
+let str = process.argv[2];
+str = str.replace(/\W/g, '');
+let c = str.replace(/\D/g, '');
+let b = str.replace(/[aeiouyAEIOUYаеёиоуыэюяАЕЁИОУЫЭЮЯ0123456789]/g,'');
+let a = str.replace(/[qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNMйцкнгшщзхъфвпрлджчсмтьбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ0123456789]/g,'');
 
-  for (let i = 0; i < str.length; i++) {
-    if (cogl.indexOf(str[i].toLowerCase()) != -1) {
-      b.push(str[i]);
-      continue;
-    }
-    if (gl.indexOf(str[i].toLowerCase()) != -1) {
-      a.push(str[i]);
-      continue;
-    }
-    if (cifr.indexOf(str[i].toLowerCase()) != -1) {
-      c.push(str[i]);
-      continue;
-    }
-  }
-  let result = "";
-  if(a.length) { result += a.join('') + " ";}
-  if(b.length) { result += b.join('') + " ";}
-  if(c.length) { result += c.join('') + " ";}
-  return result;
-}
-
-process.stdout.write(division(process.argv[2]));
+process.stdout.write(a + " " + b + " " + c);
